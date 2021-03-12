5f227c19ab7803bb550f675de946ba0bae7a1307
iGrekInLapland <velheor24@gmail.com>
2021-03-12 17:52:37 +0300

    feat: now u can downlaod img

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -28,6 +28,7 @@ function updateValue() {
   const suffix = inputs[i].dataset.sizing || '';
   document.documentElement.style.setProperty(`--${inputs[i].name}`, inputs[i].value + suffix);
 }
+drawImage()
 }
 //const btn = document.querySelectorAll('.btn')
 const reset = document.querySelector('.btn-reset');
@@ -45,7 +46,8 @@ const imageContainer = document.querySelector('.image-container');
 const base = 'https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/';
 const timeOfDay = ['night', 'morning', 'day', 'evening']
 const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
-const image = document.querySelector('img');
+//const image = document.querySelector('img');
+const image = document.getElementsByTagName('img');
 let i = 0;
 
 function viewBgImage(src) {  
@@ -55,6 +57,7 @@ function viewBgImage(src) {
   img.onload = () => {
     imageContainer.innerHTML = "";
     imageContainer.append(img);  
+    drawImage()
   }; 
 }
 
@@ -80,7 +83,8 @@ function previewFile() {
   reader.addEventListener("load", function () {
     img.src = reader.result;
     imageContainer.innerHTML = "";
-    imageContainer.append(img);  
+    imageContainer.append(img); 
+    drawImage() 
   }, false);
   if (file) {
     reader.readAsDataURL(file);
@@ -88,3 +92,30 @@ function previewFile() {
 }
 
 
+const canvas = document.querySelector('canvas');
+
+function drawImage() {
+  const img = new Image();
+  img.setAttribute('crossOrigin', 'anonymous'); 
+
+  img.src = image[0].getAttribute('src');
+  img.onload = function() {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    const ctx = canvas.getContext("2d");
+
+    ctx.filter = `blur(${inputs[0].value}px) invert(${inputs[1].value}%) sepia(${inputs[2].value}%) saturate(${inputs[3].value}%) hue-rotate(${inputs[4].value}deg)`;
+console.log(ctx.filter)
+    ctx.drawImage(img, 0, 0);
+
+  };  
+}
+drawImage()
+
+save.onclick = function(e) {
+  let link = document.createElement('a');
+  link.download = 'download.png';
+  link.href = canvas.toDataURL();
+  link.click();
+  link.delete;
+};
